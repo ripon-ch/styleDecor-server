9b45597c8a2ddf61f28026ae89d3cd7e292ca861
ripon-ch <jalalchowdhury47@gmail.com>
2025-12-11 22:39:35 +0400

    add config database and stripe

--- src/config/stripe.js
+++ src/config/stripe.js
@@ -1,4 +1,9 @@
-// src/config/stripe.js
 const Stripe = require('stripe');
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, { apiVersion: '2022-11-15' });
+
+if (!process.env.STRIPE_SECRET_KEY) {
+  console.warn('⚠️ STRIPE_SECRET_KEY not found in environment variables');
+}
+
+const stripe = Stripe(process.env.STRIPE_SECRET_KEY);
+
 module.exports = stripe;
